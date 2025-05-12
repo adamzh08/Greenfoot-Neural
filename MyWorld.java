a6ede537a6ff82c6a89d9ac077346eb38479a49f
adamzh08 <adam.zhakenov@gmail.com>
2025-05-12 20:01:08 +0200

    Refactor MyWorld class; improve time_avarage method, and streamline prepare method

--- MyWorld.java
+++ MyWorld.java
@@ -1,13 +1,13 @@
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class MyWorld here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class MyWorld extends World
-{
+public class MyWorld extends World {
 
     public static int amount_of_ants = 15;
 
@@ -16,35 +16,37 @@ public class MyWorld extends World
 
     public static long time = 0;
 
-    public static void time_avarage()
-    {
-        time = time / 10;
+    public static Ant[] ants_array = new Ant[amount_of_ants * amount_of_ants];
+
+    public static void time_avarage(int loops) {
+        for (int i1 = 0; i1 < loops; i1++) {
+            for (int i = 0; i < amount_of_ants * amount_of_ants; i++) {
+                ants_array[i].act();
+            }
+        }
+
+        time = time / loops;
         System.out.println("Time: " + time);
     }
 
     /**
      * Constructor for objects of class MyWorld.
-     * 
+     *
      */
-    public MyWorld()
-    {    
+    public MyWorld() {
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(screenWidth, screenHeight, 1); 
+        super(screenWidth, screenHeight, 1);
         prepare();
     }
+
     /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
+     * Prepare the world for the start of the program. That is: create the
+     * initial objects and add them to the world.
      */
-    private void prepare()
-    {    
-        Ant[] ants_array = new Ant[amount_of_ants*amount_of_ants];
-        
-        for(int i = 0; i < amount_of_ants*amount_of_ants; i++)
-        {
+    private void prepare() {
+        for (int i = 0; i < amount_of_ants * amount_of_ants; i++) {
             ants_array[i] = new Ant();
-            addObject(ants_array[i], (int)((screenWidth/amount_of_ants)*(int)(i/amount_of_ants)), (int)((screenHeight/amount_of_ants)*(i%amount_of_ants)));
-            //addObject(ants_array[i], 10, 10);
+            addObject(ants_array[i], (int) ((screenWidth / amount_of_ants) * (int) (i / amount_of_ants)), (int) ((screenHeight / amount_of_ants) * (i % amount_of_ants)));
         }
     }
 }
