3c1bdd6388a879ebb99f555af335bf65a776b094
Yan1889 <yan.amin256@gmail.com>
2025-05-13 14:36:44 +0200

    Cleaned up Network classes: Removed Softmax, converted Layer class to record, removed input arrayCopy in getResult() (no need, since it isn't changed)

--- Layer.java
+++ Layer.java
@@ -4,40 +4,5 @@ import java.io.*;
 /**
  * Represents a single layer in the neural network
  */
-class Layer {
-
-    /**
-     * Number of neurons in this layer
-     */
-    private int length;
-
-    /**
-     * Activation function used in this layer
-     */
-    private ActivationFunction_I activation;
-
-    /**
-     * Constructor for a neural network layer
-     *
-     * @param length Number of neurons in this layer
-     * @param activation Activation function used in this layer
-     */
-    public Layer(int length, ActivationFunction_I activation) {
-        this.length = length;
-        this.activation = activation;
-    }
-
-    /**
-     * @return Number of neurons in this layer
-     */
-    public int getLength() {
-        return length;
-    }
-
-    /**
-     * @return Activation function used in this layer
-     */
-    public ActivationFunction_I getActivation() {
-        return activation;
-    }
-}
+// class -> immutable record (automaticly generated construtor & getters)
+record Layer(int length, ActivationFunction_I activationFunction) {}
